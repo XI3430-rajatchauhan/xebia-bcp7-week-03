b72945ec4086b669c4202ed831db8411c8c594fc
XI3430-rajatchauhan <99252202+XI3430-rajatchauhan@users.noreply.github.com>
2022-03-22 20:53:22 +0530

    This is my 29 project

---
+++ HelloWorldServlet/src/com/xebia/bcp7javaee/helloworldservlet/servlets/HelloWorld.java
@@ -0,0 +1,45 @@
+package com.xebia.bcp7javaee.helloworldservlet.servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet("/HelloWorld")
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out=response.getWriter();
+		double sqrtvalue=Math.sqrt(4.0);
+		out.println("<html><body><h1 aligns='center'>Hello World Servlet</h1>"
+				+ "<br><The square root of 4.0 is: " + sqrtvalue + "<br>"
+				+ "</body></html>");
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
